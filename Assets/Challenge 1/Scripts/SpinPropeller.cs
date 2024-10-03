d9e0443c6c031b4e10aa66a950c3b561351707b2
Abrahames <abtaham0212@gmail.com>
2024-10-03 01:08:39 -0600

    primer commit

---
+++ Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+Nombre: Espinoza Gómez Abraham Salvador
+Descripción: Desafio 1
+Fecha: 01-10-2024
+*/
+public class SpinProp : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float  propellorSpeed = 1000;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward,propellorSpeed * Time.deltaTime);
+    }
+}
